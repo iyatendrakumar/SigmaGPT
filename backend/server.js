49405333a3743fc84c157f9dbcf6c4da785d7318
Yatendra Kumar <yatendrakumar18113@gmail.com>
2025-11-18 17:14:56 +0530

    Final frontend connection link added

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,7 @@ app.use(express.json());
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "https://your-frontend.vercel.app"
+    "https://sigma-gpt-lilac.vercel.app"
   ],
   methods: ["GET", "POST", "DELETE", "PUT"]
 }));
